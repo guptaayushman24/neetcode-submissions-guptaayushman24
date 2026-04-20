7671cfb8a3afade9a3a9bb49c1acc3c9b5098852
Ayushman Gupta <125189298+guptaayushman24@users.noreply.github.com>
2026-04-20 22:43:12 +0530

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.java
@@ -0,0 +1,23 @@
+class Solution {
+    public static int findNoOfWays (int n,int [] dp){
+        if (n==0 || n==1){
+             dp[n] = 1;
+            return 1;
+        }
+
+        if (dp[n]!=-1){
+            return dp[n];
+        }
+        int x = findNoOfWays(n-1,dp);
+        int y = findNoOfWays(n-2,dp);
+
+        dp[n] = x+y;
+        return x+y;
+    }
+    public int climbStairs(int n) {
+        int [] dp = new int [n+1];
+        Arrays.fill(dp,-1);
+        int ans = findNoOfWays (n,dp);
+        return ans;
+    }
+}
